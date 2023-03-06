1dd72bf85f5d286b207b8ef6ec00816fa0fd4e87
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-03-06 12:26:38 +0530

    done setting up project

---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Main(){
+    return (
+        <div className="main">
+            <h1>Tenzies</h1>
+            <p>
+            Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+            </p>
+        </div>
+    )
+}
