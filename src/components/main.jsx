08e1de9267c6f5c0cbcd6fe430dea345c33eee55
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-03-06 12:50:40 +0530

    added dice component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Dice from "./dice";
 
 export default function Main(){
+
+    let diceData = []
+
+    for (let i=0;i<10;i++){
+        diceData.push(<Dice number={i+1}/>)
+    }
+
     return (
         <div className="main">
             <h1>Tenzies</h1>
             <p>
             Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
             </p>
+            <div className="dice--grid">
+            {diceData}
+            </div>
+            
         </div>
     )
 }
